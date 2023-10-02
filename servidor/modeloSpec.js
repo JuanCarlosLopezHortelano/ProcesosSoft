4ae37e1ad87df519da1432be56b05715979b792b
JUAN CARLOS <juancarlos.lopez8@alu.uclm.es>
2023-10-02 13:06:08 +0200

    Desarrollo de pruebas

---
+++ servidor/modeloSpec.js
@@ -0,0 +1,40 @@
+const modelo = require("./modelo.js");
+const Sistema = modelo.Sistema;
+
+describe('El sistema', function() {
+   let sistema;
+   
+  beforeEach(function() {
+   sistema=new Sistema()
+
+   });
+   it('debería crear un sistema de tipo Sistema', function() {
+      expect(sistema instanceof Sistema).toBe(true);
+    });
+  
+   it('inicialmente no hay usuarios', function() {
+  expect(sistema.numeroUsuarios()).toBe(0);
+  });
+
+  it('debería agregar un usuario correctamente', function() {
+   expect(sistema.numeroUsuarios()).toEqual(0); 
+   sistema.agregarUsuario("pepe");
+    expect(sistema.numeroUsuarios()).toEqual(1);
+});
+
+ // Prueba la función eliminarUsuario
+ it('debería eliminar un usuario correctamente', function() {
+   sistema.agregarUsuario("usuario3");
+   sistema.eliminarUsuario("usuario3");
+   expect(sistema.usuarioActivo("usuario3")).toBe(false);
+});
+
+// Prueba la funcion
+it('debería devolver el número correcto de usuarios', function() {
+   sistema.agregarUsuario("usuario5");
+   sistema.agregarUsuario("usuario6");
+   expect(sistema.numeroUsuarios()).toEqual(2);
+});
+
+
+   })
